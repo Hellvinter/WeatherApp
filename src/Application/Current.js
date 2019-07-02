bc4eaa23444cfb7a57207f92867e828c9c343bf4
Hellvinter <45991026+Hellvinter@users.noreply.github.com>
2019-07-02 10:57:46 +0300

    finish working on base version of weather app

--- src/Application/Current.js
+++ src/Application/Current.js
@@ -1,18 +1,28 @@
-import React from "react"
+import React, {useState} from "react"
 import useFetch from "./utils/useFetchHook"
 
 function CurrentWeather() {
-    const inititalUrl = "http://api.apixu.com/v1/current.json?key=b2c39fc02e2844cfa9562014192606&q=beijing"
+    //set query in order to add interactive behavior
+    const [query, setQuery] = useState("vancouver")
+    // const loc create object which will contain data
+    // then I pass it like param in useFetch
+    // const place receive and store data from useFetch hook
+    const initialUrl = `http://api.apixu.com/v1/current.json?key=b2c39fc02e2844cfa9562014192606&q=${query}`
     const loc =  {current: [], location : []}
-    const place = useFetch(inititalUrl, loc)
-    //console.log(place)
-    //console.log(place.current)
-    //console.log(place.current.temp_c)
-    //console.log(place.location)
-    //console.log(place.location.name)
-
+    // by some reasonse I must pass {data} in that particular
+    // scenario for now I dont know why it's work that way
+    // need to read about that    
+    const [{data}, changeQuery] = useFetch(initialUrl, loc)
+    //console.log(data)
+    //console.log(data.current)
+    //console.log(data.current.temp_c)
+    //console.log(data.location)
+    //console.log(data.location.name)
     
-
+    // stuff below display data and whatever we want
+    // JavaScript must be written in curly brackets
+    // or it will interpreted litteraly by JSX
+    // data.location.country will be explained below
    return (
        <main className="weather_body">
             <h1 className="weather_heading">
@@ -20,22 +30,69 @@ function CurrentWeather() {
             </h1>
             <div className="weather_data">
                 <h2>Location</h2>
-                <p className="weather_data-info">
-                    Country: {place.location.country}
+                <p 
+                className="weather_data-info">
+                Country: {data.location.country}
                 </p> 
                 <p className="weather_data-info">
-                    City: {place.location.name}
+                City: {data.location.name}
                 </p>
                 <h2>Weather</h2>
-                <p className="weather_data-info">
-                    Temperature: {place.current.temp_c} celsius
+                <p 
+                className="weather_data-info">
+                Temperature: {data.current.temp_c} celsius
                 </p>
-                <p className="weather_data-info">
-                    Wind: {place.current.wind_kph} {place.current.wind_dir} 
+                <p 
+                className="weather_data-info">
+                Wind: {data.current.wind_kph} 
+                k/h {data.current.wind_dir} 
                 </p>
             </div>
+            <form
+            className="weather_search"
+            onSubmit={event => {
+                changeQuery(initialUrl);
+                event.preventDefault()
+            }}
+            >
+                <input
+                onChange={event => setQuery(event.target.value)}
+                className="weather_search-text" 
+                type="text" placeholder="Choose city"   
+                />
+                <button 
+                className="weather_search-btn" 
+                type="submit">
+                Search
+                </button>
+            </form>
        </main>
    ) 
 }
 
 export default CurrentWeather
+
+// This whole componennt take data from apixu.com API 
+// by using custom fetch hook and then display it
+
+// Explanation
+// This component display weather data on screen
+// But before it display it it use custom hook useFetch
+// Take data from an API and only then display it
+// we grand access to data with object methods with dot notation
+// first we take const contain data : {data} then add dot to it
+// like this {data}.
+// then we need go further in our object we need take access
+// to nested object so after dot we writting name of that child object
+// data.current for example
+// then we need take value of particular key so
+// we writte a name of key which value we needed
+// like this data.current.temp_c and than data displayed 
+// to screen
+
+// I add query state in order to create some interactive action
+// and ability to check weather in city you want
+// I made it by onChange and onSubmit events in form
+// onChange event setQuery in [query, setQuery]
+// and onSubmit update our initialUrl so displayed data
+// is changed by our demand  
--- src/Application/utils/useFetchHook.js
+++ src/Application/utils/useFetchHook.js
@@ -2,17 +2,19 @@ import {useState, useEffect} from "react"
 import axios from "axios"
 
 function useFetch(initUrl, initData) {
-    const [data, setData] =useState(initData)
+    const [data, setData] = useState(initData)
+    const [url, setUrl] = useState(initUrl)
 
     useEffect(() => {
         const Fetch = async() => {
-            const result = await axios(initUrl)
+            const result = await axios(url)
             setData(result.data)
         }
-        Fetch()
-    },[])
 
-    return data
+        Fetch()
+    },[url])
+
+    return [{data}, setUrl]
 }
 
 export default useFetch
@@ -37,7 +39,7 @@ export default useFetch
 // it's initUrl and initData
 // because of that params hook became reusable
 // Then I declair const [data, setData] 
-//with destructuring method and with asign to that
+// with destructuring method and with asign to that
 // const useState hook with param initData
 // that param will be passed when our custom hook is called
 // then I use useEffect to fetch data 
@@ -45,6 +47,39 @@ export default useFetch
 // that wai we can use any url in any component we need
 // when coll useFetch function
 // then for second param of useEffect 
-//I use emty squere brackets for now
+// I use emty squere brackets for now
 // letter i change it to query (that way it will change
 // fetched data when query is changed)
+
+
+// Planning 
+// Today 02.07.19 I need to improve that hook
+// by adding to it ability to fetch data by demand
+// for example it's will be search form 
+// I need somehow give ability to change initialUrl
+// for thea sake, I think need create one more const
+// [url, setUrl] in order to have ability to change it
+// apperently const with url is working
+// so for now I fink that I need some how to pass 
+// set url function to my Current component
+// but how I will do it?
+// maybe destructor and pass data as object and setUrl in 
+// same array
+// But then what?
+// how I make it works
+// it will be a part of useFetch anyway
+// so can I just pass it like parametr
+// WRONG
+// I need use useState in Current component to set query
+// also I need return setUrl in return of useFetchHooks
+// then I need to change const place in Current a little bit
+// like that [{data}, changeUrl] = useFetch(param1,param2)
+// then change place on data everywhere or all things will broke
+// and then I need to create form which will change query
+// and in submit I call changeUrl so it change initUrl
+// before that I need add onChange to input type="text"
+// onChange={event => setQuery(event.target.value)}
+// and in submit onSubmit={event => {
+// changeUrl(url=${query}); event.preventDefault() (like that
+// page don't reload)
+//}}
