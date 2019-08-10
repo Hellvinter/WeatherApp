11b55d1a3303d1d9cba8a80c33a6b82a22cbc1c1
Hellvinter <45991026+Hellvinter@users.noreply.github.com>
2019-08-10 16:16:37 +0300

    reinstall react-scripts

---
+++ src/Application/App.js
@@ -0,0 +1,34 @@
+import React  from "react";
+
+import CurrentWeather from "./Current";
+
+function App() {
+  return (
+    <div className="App">
+      <CurrentWeather />
+    </div>
+    
+  );
+}
+
+export default App;
+
+// This component its  a core of App
+// It's contain all our components
+// And then our index.js take all of this
+// stuff and render in div with id="root"
+// After that magic hapens and all our code
+// displayed on screen
+
+//Daily component
+// First call state of DailyTemp function then .map it
+// pass parameters starship and index in order to take data from
+// object and iterate through different object childs
+// then call DeathStar const and pass arguments to it key=index
+// index=index, starship=starship, then magic happen
+
+//Stuff you written above...
+// Dude how dum I can be sometime
+// You need to create const with jsx markup inside DailyTemp
+// and pass it within DailyTemp component in return
+// In App you just call it
---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import "./Application/styles.css";
+
+import App from "./Application/App";
+
+const rootElement = document.getElementById('root');
+ReactDOM.render(<App />, rootElement);
+
