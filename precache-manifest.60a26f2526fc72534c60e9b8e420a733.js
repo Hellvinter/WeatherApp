0757a0b151cb584834aed956877cb8f19caa8b33
Hellvinter <45991026+Hellvinter@users.noreply.github.com>
2019-08-15 11:37:24 +0300

    Updates

---
+++ precache-manifest.60a26f2526fc72534c60e9b8e420a733.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "729b422977cc8f46a9915167c6e82707",
+    "url": "/WeatherApp/index.html"
+  },
+  {
+    "revision": "1dd0793c2e0b6a254a45",
+    "url": "/WeatherApp/static/css/main.a6dbed8a.chunk.css"
+  },
+  {
+    "revision": "4c9e7eeafee8a9d8a06c",
+    "url": "/WeatherApp/static/js/2.44bfe881.chunk.js"
+  },
+  {
+    "revision": "1dd0793c2e0b6a254a45",
+    "url": "/WeatherApp/static/js/main.882da3e9.chunk.js"
+  },
+  {
+    "revision": "ee49d575ee71513e1b3b",
+    "url": "/WeatherApp/static/js/runtime~main.b79fbe78.js"
+  }
+]);
